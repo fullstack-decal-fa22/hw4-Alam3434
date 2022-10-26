7ff5151417132ead4ab416cae59ed59f35324f0f
Mohammad Alam <malam2003@berkeley.edu>
2022-10-25 20:54:03 -0700

    worked

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import { useState } from 'react';
+import Block from './Block';
+import Color from './Color';
 /* Add any imports you think you might need here! */
 
-const Menu = () => { 
-
+const Menu = (props) => { 
+  const [name, setColor]= useState(false)
+  
+    
+  
     return (
-      <div className="colorOptions">
-          {/* TODO */}
+      <div className="colorOptions" >
+          <Color color="red" handleClick={props.func}/>
+          <Color color="blue" handleClick={props.func}/>
+          <Color color="green" handleClick={props.func}/>
+          <Color color="yellow" handleClick={props.func}/>
       </div>
     );
-}
+   }
 
 export default Menu;
